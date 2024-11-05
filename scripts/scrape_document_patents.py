4b9ec21147ab212d5f7b9dab6a18301a1f67fb7f
Federico Galatolo <galatolo.federico@gmail.com>
2024-11-05 06:43:01 +0000

    scrape up to 5 documents for query than break

--- scripts/scrape_document_patents.py
+++ scripts/scrape_document_patents.py
@@ -12,6 +12,7 @@ def scrape_documents_from_query(json_file_path, front_imgs_dir_output, json_dir_
     - json_file_path (str): The path to the input JSON file containing patent data of a query patent.
     - front_imgs_dir_output (str): Directory where front images of document patents will be saved.
     - json_dir_output (str): Directory where patent data of the document patents  will be saved as a JSON file.
+    - sample_size (int): number of documents to be scraped from the citation list of the query
 
     This function reads patent query data from a JSON file, extracts citation information.
     Then, for each cited patent (document patentens), it scrapes patent data and downloads the corresponding front image.
@@ -34,12 +35,12 @@ def scrape_documents_from_query(json_file_path, front_imgs_dir_output, json_dir_
         query_ID = os.path.splitext(os.path.basename(json_file_path))[0]
         query_data = json.load(file)
         citations_list = query_data.get('citations_by_examiner') # Retrieve the list of patent IDs cited by the examiner.
-        print(f'\nStarting scraping for query: {query_ID}')
+        print(f'\nScraping query: {query_ID} ...')
 
         # Check if citations list is not empty
-        if len(citations_list) >= sample_size:
+        if len(citations_list) >= sample_size: # either 0 or sample_size
             random.seed(1999) 
-            random.shuffle(citations_list) # shuflle the citations lists to iterate randomly over the citation list.
+            random.shuffle(citations_list) # shuflle the citations lists to iterate randomly over the citation list
             scraped_count = 0 # initialize counter for successfully scraped patents
             #citations_to_sample = min(len(citations_list), sample_size) # if the citation list has fewer elements than sample_size, it will sample the entire list
             #citations_list_rand = random.sample(citations_list, citations_to_sample) # get random elements from the citation list
@@ -56,6 +57,11 @@ def scrape_documents_from_query(json_file_path, front_imgs_dir_output, json_dir_
                     scraped_count += 1
                     continue
 
+                # Necessary when resuming scraping
+                if scraped_count >= sample_size:
+                    print(f"Successfully scraped {scraped_count} patents, stopping further scraping for this query.")
+                    break
+                
                 # List of scraping functions with their arguments 
                 scraping_functions = [
                     (get_abstract, driver, url),
@@ -93,6 +99,7 @@ def scrape_documents_from_query(json_file_path, front_imgs_dir_output, json_dir_
                     # Write the document patent data dictionary to a JSON file
                     with open(json_filepath, 'w') as json_file:
                         json.dump(document_patent_data, json_file, indent=2)
+                        print(f"{doc_ID} successfully scraped.")
                     
                     # Increment the scraped count and check if target is reached
                     scraped_count += 1
@@ -100,12 +107,13 @@ def scrape_documents_from_query(json_file_path, front_imgs_dir_output, json_dir_
                         print(f"Successfully scraped {scraped_count} patents, stopping further scraping for this query.")
                         break
                 else:
-                    print(f"{patent_ID} from: {url} not succesfully scraped due to an earlier failure.")
-
-            print(f"Scraping completed for query: {query_ID}")  
+                    print(f"{doc_ID} from: {url} not succesfully scraped due to an earlier failure.")
+            
+            print(f"Scraping completed. Successfully scraped {scraped_count} patents.")  
 
         else:
-            print(f'No citations by examiner found for: {query_ID}')
+            print(f'Scraping completed. Citations by examiner are less than {sample_size} for: {query_ID}')
+            #print(f'Scraping completed. No citations found for: {query_ID}')
 
 
 if __name__ == "__main__":
@@ -118,7 +126,7 @@ if __name__ == "__main__":
                         help='Directory to save front images of the document patents.')
     parser.add_argument('--json_dir_output', type=str, default='/vast/marco/Data_Google_Patent/json/document',
                         help='Directory to save JSON files of the document patents.')
-    parser.add_argument('--CPC_to_exclude', type=list, default=['A42B3', 'A62B18', 'F04D17', 'F16H1', 'F16L1', 'G02C5'],
+    parser.add_argument('--CPC_to_exclude', type=list, default=['H02K19'],
                         help="CPC file to exclude when resuming scraping. Example: ['A42B3', 'A62B18', 'F04D17', 'F16H1', 'F16L1', 'G02C5','H02K19']")
     args = parser.parse_args()  
 
@@ -131,7 +139,7 @@ for CPC_dir in os.listdir(args.json_dir_input):
         continue
     
     # Iterate through each JSON file in the current CPC directory.
-    print(f'\nStarting scraping for CPC: {CPC_dir}')
+    print(f'\nStarting scraping for CPC: {CPC_dir} ...')
     for json_file in os.listdir(CPC_dir_path):
         json_file_path = os.path.join(CPC_dir_path, json_file)
         scrape_documents_from_query(json_file_path, args.front_imgs_dir_output, args.json_dir_output)
